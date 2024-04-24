d4ea3b59960c8a407cdd9238c4bafcb71ac9242c
Austin-Brock <135121147+Austin-Brock@users.noreply.github.com>
2024-04-24 16:17:07 -0400

    updated code for testing

--- parse_traceroute.py
+++ parse_traceroute.py
@@ -1,7 +1,8 @@
-import random
 import re
-# Corrected function to parse the network trace dump
-def parse_network_trace_corrected(dump_lines):
+import random
+
+# Function to parse the network trace dump
+def parse_network_trace(dump_lines):
     data = {}
     for line in dump_lines:
         if 'IP' in line:
@@ -24,3 +25,27 @@ def parse_network_trace_corrected(dump_lines):
                 data[ttl]['times'].append(response_times)
 
     return data
+
+# Function to print the formatted output
+def print_formatted_output(data):
+    for ttl, details in data.items():
+        print(f"TTL {ttl}")
+        for ip, times in zip(details['ips'], details['times']):
+            print(ip)
+            for time in times:
+                print(time)
+
+# Sample input from the network trace dump
+sample_dump_lines = [
+    '1296184417.509580 IP (tos 0x0, ttl 1, id 30632, offset 0, flags [none], proto TCP (6), length 60)',
+    '    128.192.76.178.35952 > 212.58.244.68.80: Flags [SEW], cksum 0xc2c4 (correct), seq 1857692485, win 5840, options [mss 1460,sackOK,TS val 1090680828 ecr 0,nop,wscale 2], length 0',
+    # Additional sample lines for demonstration
+    '1296184417.509599 IP (tos 0x0, ttl 2, id 30633, offset 0, flags [none], proto TCP (6), length 60)',
+    '    128.192.76.178.38611 > 212.58.244.68.80: Flags [SEW], cksum 0x97a4 (correct), seq 3741111231, win 5840, options [mss 1460,sackOK,TS val 1090680828 ecr 0,nop,wscale 2], length 0',
+]
+
+# Parsing the dump and preparing the formatted output
+parsed_data = parse_network_trace(sample_dump_lines)
+print_formatted_output(parsed_data)
+
+
